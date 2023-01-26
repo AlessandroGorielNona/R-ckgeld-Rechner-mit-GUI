e079e62c31ea33bd77b9b6b91abc023a3621c9ce
Alessandro Goriel Nona <100690700+AlessandroGorielNona@users.noreply.github.com>
2023-01-26 18:37:12 +0100

    Update rückgeldrechnerGUI.py

--- rückgeldrechnerGUI.py
+++ rückgeldrechnerGUI.py
@@ -6,26 +6,26 @@ def calculate_change():
         cash_received = float(cash_received_entry.get())
         change = cash_received - amount_due
         if change < 0:
-            change_label.config(text="Warning: Insufficient payment", fg="red")
+            change_label.config(text="Warnung: Zu wenig bezahlt!", fg="red")
         else:
-            change_label.config(text=f"Change: {change}", fg="black")
+            change_label.config(text=f"Wechseln: {change}", fg="black")
     except ValueError:
         change_label.config(text="Invalid input", fg="red")
 
 root = tk.Tk()
-root.title("Change Calculator")
+root.title("Rückgeld Rechner")
 
-amount_due_label = tk.Label(root, text="Amount Due:")
+amount_due_label = tk.Label(root, text="Zu zahlen:")
 amount_due_label.grid(row=0, column=0)
 amount_due_entry = tk.Entry(root)
 amount_due_entry.grid(row=0, column=1)
 
-cash_received_label = tk.Label(root, text="Cash Received:")
+cash_received_label = tk.Label(root, text="Geld erhalten:")
 cash_received_label.grid(row=1, column=0)
 cash_received_entry = tk.Entry(root)
 cash_received_entry.grid(row=1, column=1)
 
-calculate_button = tk.Button(root, text="Calculate", command=calculate_change)
+calculate_button = tk.Button(root, text="Rechnen", command=calculate_change)
 calculate_button.grid(row=2, column=0, columnspan=2, pady=10)
 
 change_label = tk.Label(root, text="Change:")
